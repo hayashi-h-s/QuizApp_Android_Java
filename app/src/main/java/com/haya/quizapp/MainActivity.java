7e71feb1bf4ad7e73cb92a80c5e6b17e0aca0148
Haya <max9173627@gmail.com>
2020-07-05 12:45:20 +0900

    [add]レイアウトとボタンクリック時の挙動を追加

--- app/src/main/java/com/haya/quizapp/MainActivity.java
+++ app/src/main/java/com/haya/quizapp/MainActivity.java
@@ -1,14 +1,51 @@
 package com.haya.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnTrue;
+    private Button btnWrong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnTrue = findViewById(R.id.btnTrue);
+
+        btnTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                evaluateUsersAnswer(true);
+//                changeQuestionOnButtonClick();
+                Log.i("クリック表示：","本当ボタン");
+
+            }
+        });
+
+        btnWrong = findViewById(R.id.btnWrong);
+
+        //  btnWrong.setOnClickListener(myClickListener);
+
+        btnWrong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                evaluateUsersAnswer(false);
+//                changeQuestionOnButtonClick();
+                Log.i("クリック表示：","嘘ボタン");
+
+            }
+        });
+
+
     }
+
 }
